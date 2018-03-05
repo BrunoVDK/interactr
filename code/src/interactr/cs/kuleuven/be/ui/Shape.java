410e7ceebd1f7831f255a11ac2d2c1be6c2325d7
Bruno Vandekerkhove <brunovandekerkhove@protonmail.com>
2018-03-05 11:04:16 +0100

    Update

--- code/src/interactr/cs/kuleuven/be/ui/Shape.java
+++ code/src/interactr/cs/kuleuven/be/ui/Shape.java
@@ -2,10 +2,6 @@ package interactr.cs.kuleuven.be.ui;
 
 public interface Shape {
 
-<<<<<<< HEAD
     public void draw();
-=======
-    //public draw();
->>>>>>> 0f9ff999d954b893f2320f09f91f4b4c15e838cd
 
 }
