33c0ae7f247c2648b256e51276c29024f9b2ceb9
HannesDS <HannesDeSmet@MacBook-Pro-van-Hannes.local>
2018-03-08 17:01:09 +0100

    nothing to commit

--- code/src/test/interactr/cs/kuleuven/be/UseCases/AddPartyTest.java
+++ code/src/test/interactr/cs/kuleuven/be/UseCases/AddPartyTest.java
@@ -1,12 +1,12 @@
 package interactr.cs.kuleuven.be.UseCases;
 
-import interactr.cs.kuleuven.be.domain.Diagram;
-import interactr.cs.kuleuven.be.ui.DiagramController;
-import interactr.cs.kuleuven.be.ui.DiagramWindow;
-import interactr.cs.kuleuven.be.ui.EventHandler;
-import interactr.cs.kuleuven.be.ui.PaintBoard;
+import interactr.cs.kuleuven.be.domain.Party;
+import interactr.cs.kuleuven.be.ui.*;
+import interactr.cs.kuleuven.be.ui.exceptions.InvalidAddPartyException;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class AddPartyTest {
 
@@ -22,26 +22,54 @@ public class AddPartyTest {
         eventHandler = new EventHandler(diagramController);
         diagramWindow.setEventHandler(new EventHandler(new DiagramController()));
         diagramWindow.setPaintBoard(new PaintBoard(diagramWindow, diagramWindow.getEventHandler().getDiagramController()));
-        diagra
+
     }
 
     @Test
     void addPartyToEmptyAreaSequence(){
-
+        DiagramWindow.("doubleClick.txt" , diagramWindow);
+        assertEquals(1,diagramController.getDiagram().getParties().size());
     }
 
     @Test
     void addPartyToOccupiedAreaSequence(){
+        DiagramWindow.replayRecording("doubleClick.txt" , diagramWindow);
+        Party firstAdded = diagramController.getDiagram().getParties().getFirst();
+        try{
+            DiagramWindow.replayRecording("doubleClick.txt" , diagramWindow);
+            assert(false);
+        }catch (InvalidAddPartyException e){
+            assertEquals(e.getParty(),firstAdded);
+        }
+    }
 
+    @Test
+    void addPartyToForbiddenAreaSequence(){
+        try {
+            DiagramWindow.replayRecording("doubleClickMiddle.txt", diagramWindow);
+        }catch (InvalidAddPartyException e){
+            assertEquals(e.getParty(),null);
+        }
     }
 
     @Test
     void addPartyToEmptyAreaCommunication(){
+        diagramController.nextView();
+        DiagramWindow.replayRecording("doubleClick.txt" , diagramWindow);
+        assertEquals(1,diagramController.getDiagram().getParties().size());
 
     }
 
     @Test
     void addPartyToOccupiedAreaCommunication(){
-
+        diagramController.nextView();
+        DiagramWindow.replayRecording("doubleClick.txt" , diagramWindow);
+        Party firstAdded = diagramController.getDiagram().getParties().getFirst();
+        try{
+            DiagramWindow.replayRecording("doubleClick.txt" , diagramWindow);
+            assert(false);
+        }catch (InvalidAddPartyException e){
+            assertEquals(e.getParty(),firstAdded);
+        }
     }
 }
