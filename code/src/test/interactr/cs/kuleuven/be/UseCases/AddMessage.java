abd6062a603acfd06467684a122d8a59ab40391d
HannesDS <HannesDeSmet@MacBook-Pro-van-Hannes.local>
2018-03-09 02:25:35 +0100

    added some last tests

---
+++ code/src/test/interactr/cs/kuleuven/be/UseCases/AddMessage.java
@@ -0,0 +1,45 @@
+package interactr.cs.kuleuven.be.UseCases;
+
+import interactr.cs.kuleuven.be.ui.DiagramController;
+import interactr.cs.kuleuven.be.ui.DiagramWindow;
+import interactr.cs.kuleuven.be.ui.EventHandler;
+import interactr.cs.kuleuven.be.ui.PaintBoard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddMessage {
+
+    private DiagramWindow diagramWindow = new DiagramWindow();
+
+
+    @BeforeEach
+    void setUp(){
+        diagramWindow.setEventHandler(new EventHandler(new DiagramController()));
+        diagramWindow.setPaintBoard(new PaintBoard(diagramWindow, diagramWindow.getEventHandler().getDiagramController()));
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        DiagramWindow.replayRecording("addPartyLegalLabel.txt", diagramWindow);
+        DiagramWindow.replayRecording("addPartyLegalLabel100X.txt", diagramWindow);;
+
+    }
+
+    @Test
+    void addMessageBetweenTwoPartiesSequence(){
+        DiagramWindow.replayRecording("addMessage.txt",diagramWindow);
+        assertEquals(2,diagramWindow.getEventHandler().getDiagramController().getDiagram().getMessages().size());
+    }
+
+    @Test
+    void moveWithMessagesSequence(){
+        DiagramWindow.replayRecording("addMessage.txt",diagramWindow);
+        DiagramWindow.replayRecording("moveTill200.txt",diagramWindow);
+        assertEquals(2,diagramWindow.getEventHandler().getDiagramController().getDiagram().getMessages().size());
+    }
+
+
+}
