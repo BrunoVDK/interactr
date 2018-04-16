1e124c83bcaadc300dc327969b60f919a98d2cff
Bruno Vandekerkhove <brunovandekerkhove@protonmail.com>
2018-04-16 16:20:13 +0200

    Update Tests

--- code/src/test/UseCases/CloseWindow.java
+++ code/src/test/UseCases/CloseWindow.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class CloseWindow {
     private DiagramWindow diagramWindow = new DiagramWindow();
 
