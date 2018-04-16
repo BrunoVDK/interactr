1f354c8e010d6a6b8982d42feecd190c768fe6e2
Bruno Vandekerkhove <brunovandekerkhove@protonmail.com>
2018-04-16 20:07:44 +0200

    Added DiagramControllerTest

---
+++ code/src/test/unit/ui/DiagramControllerTest.java
@@ -0,0 +1,20 @@
+package unit.ui;
+
+import interactr.cs.kuleuven.be.domain.Message;
+import interactr.cs.kuleuven.be.domain.Party;
+import interactr.cs.kuleuven.be.exceptions.NoSuchWindowException;
+import interactr.cs.kuleuven.be.ui.DiagramController;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class DiagramControllerTest {
+
+    @Test
+    void activateSubWindowTest() {
+        DiagramController controller = new DiagramController();
+        assertThrows(NoSuchWindowException.class,
+                ()->{controller.activateSubWindow(0,0);});
+    }
+
+}
