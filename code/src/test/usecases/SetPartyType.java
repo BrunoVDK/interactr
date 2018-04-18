a0b3e15cbda26b034d7ba340e1798cf4bfa856c1
Bruno Vandekerkhove <brunovandekerkhove@protonmail.com>
2018-04-18 14:42:32 +0200

    Added Set Party Type Step-By-Step

--- code/src/test/usecases/SetPartyType.java
+++ code/src/test/usecases/SetPartyType.java
@@ -2,6 +2,7 @@ package usecases;
 
 import interactr.cs.kuleuven.be.domain.ActorParty;
 import interactr.cs.kuleuven.be.domain.ObjectParty;
+import interactr.cs.kuleuven.be.domain.Party;
 import interactr.cs.kuleuven.be.ui.DiagramController;
 import interactr.cs.kuleuven.be.ui.DiagramWindow;
 import interactr.cs.kuleuven.be.ui.EventHandler;
@@ -9,7 +10,7 @@ import interactr.cs.kuleuven.be.ui.PaintBoard;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SetPartyType {
 
@@ -19,6 +20,31 @@ public class SetPartyType {
     void setUp(){
         diagramWindow.setEventHandler(new EventHandler(new DiagramController()));
         diagramWindow.setPaintBoard(new PaintBoard(diagramWindow, diagramWindow.getEventHandler().getDiagramController()));
+    }
+
+    @Test
+    void stepByStepTest() {
+        DiagramController controller = diagramWindow.getEventHandler().getDiagramController();
+        // Precondition
+        DiagramWindow.replayRecording("steps/createNewDiagram.txt",diagramWindow);
+        assertNotNull(controller.getActiveSubwindow());
+        DiagramWindow.replayRecording("steps/createParty.txt",diagramWindow);
+        assertTrue(controller.getActiveSubwindow().getDiagram().getParties().size() > 0);
+        // Switch type
+        Party newParty = controller.getActiveSubwindow().getDiagram().getParties().get(0);
+        assertEquals(controller.getActiveSubwindow().getSelectedComponent(), newParty);
+        // Type label
+        DiagramWindow.replayRecording("steps/typePartyLabel.txt",diagramWindow);
+        assertEquals(controller.getActiveSubwindow().getSelectedLabel(), "a:A");
+        assertEquals(newParty.getLabel(), "a:A");
+        // Press enter
+        DiagramWindow.replayRecording("steps/pressEnter.txt",diagramWindow);
+        assertNull(controller.getActiveSubwindow().getSelectedComponent());
+        assertEquals(newParty.getLabel(), "a:A");
+        // Switch type
+        DiagramWindow.replayRecording("steps/switchPartyType.txt",diagramWindow);
+        Party switchedParty = controller.getActiveSubwindow().getDiagram().getParties().get(0);
+        assertNotEquals(switchedParty, newParty);
     }
 
     @Test
