007724f6a5555c0970cc2bd6a7a97a49176d8b8c
jelle <jdeconinck42@gmail.com>
2018-04-18 10:45:47 +0200

    Port purecollections test over to JUnit 5

--- code/src/test/unit/purecollections/PListTest.java
+++ code/src/test/unit/purecollections/PListTest.java
@@ -6,8 +6,8 @@ import java.util.ListIterator;
 import java.util.NoSuchElementException;
 
 import interactr.cs.kuleuven.be.purecollections.PList;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -29,74 +29,74 @@ public class PListTest {
 		for (int i = 0; i < 1000000; i++)
 			list = list.plus(i);
 		for (int i = 0; i < 1000000; i++) {
-			Assert.assertEquals(i, (int)list.getFirst());
+			Assertions.assertEquals(i, (int)list.getFirst());
 			list = list.minusFirst();
 		}
-		Assert.assertSame(PList.empty(), list);
+		Assertions.assertSame(PList.empty(), list);
 	}
 	
 	@Test
 	public void testSecondaryMethods() {
 		PList<Integer> list = PList.<Integer>empty().plus(1).plus(2).plus(3).plus(4);
 		
-		Assert.assertEquals("[1, 2, 3, 4]", list.toString());
-		Assert.assertEquals(Arrays.asList(new Integer[] {1,2,3,4}), list);
-		Assert.assertEquals(list, Arrays.asList(new Integer[] {1,2,3,4}));
-		Assert.assertEquals(Arrays.asList(new Integer[] {1,2,3,4}).hashCode(), list.hashCode());
-		Assert.assertFalse(list.equals(Arrays.asList(new Integer[] {1,2,3})));
-		Assert.assertFalse(list.equals(Arrays.asList(new Integer[] {1,2,3,5})));
+		Assertions.assertEquals("[1, 2, 3, 4]", list.toString());
+		Assertions.assertEquals(Arrays.asList(new Integer[] {1,2,3,4}), list);
+		Assertions.assertEquals(list, Arrays.asList(new Integer[] {1,2,3,4}));
+		Assertions.assertEquals(Arrays.asList(new Integer[] {1,2,3,4}).hashCode(), list.hashCode());
+		Assertions.assertFalse(list.equals(Arrays.asList(new Integer[] {1,2,3})));
+		Assertions.assertFalse(list.equals(Arrays.asList(new Integer[] {1,2,3,5})));
 		
-		Assert.assertEquals(4, list.size());
-		Assert.assertFalse(list.isEmpty());
+		Assertions.assertEquals(4, list.size());
+		Assertions.assertFalse(list.isEmpty());
 		Integer[] xs = {1, 2, 3, 4};
-		Assert.assertArrayEquals(xs, new ArrayList<Integer>(list).toArray()); // tests the iterator
-		Assert.assertArrayEquals(xs, list.toArray());
-		Assert.assertArrayEquals(xs, list.toArray(new Integer[0]));
-		Assert.assertArrayEquals(new Integer[] {1,2,4}, list.minus((Integer)3).toArray());
-		Assert.assertArrayEquals(new Integer[] {1,3,4}, list.minus(1).toArray());
-		Assert.assertArrayEquals(new Integer[] {1,0,2,3,4}, list.plus(1, 0).toArray());
-		Assert.assertArrayEquals(new Integer[] {0,1,2,3,4}, list.plus(0, 0).toArray());
-		Assert.assertArrayEquals(new Integer[] {0,1,2,3,4}, list.plusFirst(0).toArray());
-		Assert.assertArrayEquals(new Integer[] {-2,-1,0,1,2,3,4}, list.plusFirst(0).plusFirst(-1).plusFirst(-2).toArray());
-		Assert.assertArrayEquals(new Integer[] {1,2,5,4}, list.with(2, 5).toArray());
-		Assert.assertArrayEquals(new Integer[] {1,2,3,4,5,6}, list.plusAll(Arrays.asList(new Integer[]{5,6})).toArray());
-		Assert.assertArrayEquals(new Integer[] {1,3}, list.minusAll(Arrays.asList(new Integer[]{2,4})).toArray());
-		Assert.assertArrayEquals(new Integer[] {2,3}, list.subList(1,3).toArray());
-		Assert.assertEquals(0, list.indexOf(1));
-		Assert.assertEquals(-1, list.indexOf(5));
-		Assert.assertEquals(4, list.plus(1).lastIndexOf(1));
-		Assert.assertEquals(-1, list.lastIndexOf(5));
-		Assert.assertTrue(list.contains(3));
-		Assert.assertFalse(list.contains(-100));
-		Assert.assertEquals((Object)2, list.get(1));
-		Assert.assertTrue(list.containsAll(Arrays.asList(new Integer[] {1, 4})));
-		Assert.assertFalse(list.containsAll(Arrays.asList(new Integer[] {1, 5})));
+		Assertions.assertArrayEquals(xs, new ArrayList<Integer>(list).toArray()); // tests the iterator
+		Assertions.assertArrayEquals(xs, list.toArray());
+		Assertions.assertArrayEquals(xs, list.toArray(new Integer[0]));
+		Assertions.assertArrayEquals(new Integer[] {1,2,4}, list.minus((Integer)3).toArray());
+		Assertions.assertArrayEquals(new Integer[] {1,3,4}, list.minus(1).toArray());
+		Assertions.assertArrayEquals(new Integer[] {1,0,2,3,4}, list.plus(1, 0).toArray());
+		Assertions.assertArrayEquals(new Integer[] {0,1,2,3,4}, list.plus(0, 0).toArray());
+		Assertions.assertArrayEquals(new Integer[] {0,1,2,3,4}, list.plusFirst(0).toArray());
+		Assertions.assertArrayEquals(new Integer[] {-2,-1,0,1,2,3,4}, list.plusFirst(0).plusFirst(-1).plusFirst(-2).toArray());
+		Assertions.assertArrayEquals(new Integer[] {1,2,5,4}, list.with(2, 5).toArray());
+		Assertions.assertArrayEquals(new Integer[] {1,2,3,4,5,6}, list.plusAll(Arrays.asList(new Integer[]{5,6})).toArray());
+		Assertions.assertArrayEquals(new Integer[] {1,3}, list.minusAll(Arrays.asList(new Integer[]{2,4})).toArray());
+		Assertions.assertArrayEquals(new Integer[] {2,3}, list.subList(1,3).toArray());
+		Assertions.assertEquals(0, list.indexOf(1));
+		Assertions.assertEquals(-1, list.indexOf(5));
+		Assertions.assertEquals(4, list.plus(1).lastIndexOf(1));
+		Assertions.assertEquals(-1, list.lastIndexOf(5));
+		Assertions.assertTrue(list.contains(3));
+		Assertions.assertFalse(list.contains(-100));
+		Assertions.assertEquals((Object)2, list.get(1));
+		Assertions.assertTrue(list.containsAll(Arrays.asList(new Integer[] {1, 4})));
+		Assertions.assertFalse(list.containsAll(Arrays.asList(new Integer[] {1, 5})));
 		
 		ListIterator<Integer> iterator = list.listIterator();
-		Assert.assertEquals(0, iterator.nextIndex());
-		Assert.assertEquals(-1, iterator.previousIndex());
-		Assert.assertEquals(true, iterator.hasNext());
-		Assert.assertEquals(false, iterator.hasPrevious());
-		Assert.assertEquals((Integer)1, iterator.next());
-		Assert.assertEquals(1, iterator.nextIndex());
-		Assert.assertEquals(0, iterator.previousIndex());
-		Assert.assertEquals(true, iterator.hasNext());
-		Assert.assertEquals(true, iterator.hasPrevious());
-		Assert.assertEquals((Integer)1, iterator.previous());
-		Assert.assertEquals(0, iterator.nextIndex());
+		Assertions.assertEquals(0, iterator.nextIndex());
+		Assertions.assertEquals(-1, iterator.previousIndex());
+		Assertions.assertEquals(true, iterator.hasNext());
+		Assertions.assertEquals(false, iterator.hasPrevious());
+		Assertions.assertEquals((Integer)1, iterator.next());
+		Assertions.assertEquals(1, iterator.nextIndex());
+		Assertions.assertEquals(0, iterator.previousIndex());
+		Assertions.assertEquals(true, iterator.hasNext());
+		Assertions.assertEquals(true, iterator.hasPrevious());
+		Assertions.assertEquals((Integer)1, iterator.previous());
+		Assertions.assertEquals(0, iterator.nextIndex());
 		
 		iterator = list.listIterator(4);
-		Assert.assertEquals(4, iterator.nextIndex());
-		Assert.assertEquals(3, iterator.previousIndex());
-		Assert.assertEquals(false, iterator.hasNext());
-		Assert.assertEquals(true, iterator.hasPrevious());
-		Assert.assertEquals((Integer)4, iterator.previous());
-		Assert.assertEquals(3, iterator.nextIndex());
-		Assert.assertEquals(2, iterator.previousIndex());
-		Assert.assertEquals(true, iterator.hasNext());
-		Assert.assertEquals(true, iterator.hasPrevious());
-		Assert.assertEquals((Integer)4, iterator.next());
-		Assert.assertEquals(4, iterator.nextIndex());
+		Assertions.assertEquals(4, iterator.nextIndex());
+		Assertions.assertEquals(3, iterator.previousIndex());
+		Assertions.assertEquals(false, iterator.hasNext());
+		Assertions.assertEquals(true, iterator.hasPrevious());
+		Assertions.assertEquals((Integer)4, iterator.previous());
+		Assertions.assertEquals(3, iterator.nextIndex());
+		Assertions.assertEquals(2, iterator.previousIndex());
+		Assertions.assertEquals(true, iterator.hasNext());
+		Assertions.assertEquals(true, iterator.hasPrevious());
+		Assertions.assertEquals((Integer)4, iterator.next());
+		Assertions.assertEquals(4, iterator.nextIndex());
 	}
 	
 }
--- code/src/test/unit/purecollections/PMapTest.java
+++ code/src/test/unit/purecollections/PMapTest.java
@@ -12,8 +12,8 @@ import java.util.Set;
 
 import interactr.cs.kuleuven.be.purecollections.PList;
 import interactr.cs.kuleuven.be.purecollections.PMap;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class PMapTest {
 
@@ -23,10 +23,10 @@ public class PMapTest {
 		for (int i = 0; i < 1000000; i++)
 			map = map.plus(i, i);
 		for (int i = 0; i < 1000000; i++) {
-			Assert.assertEquals(i, (int)map.get(i));
+			Assertions.assertEquals(i, (int)map.get(i));
 			map = map.minus(i);
 		}
-		Assert.assertSame(PMap.empty(), map);
+		Assertions.assertSame(PMap.empty(), map);
 	}
 	
 	@Test
@@ -40,10 +40,10 @@ public class PMapTest {
 		random = new Random(0);
 		for (int i = 0; i < 1000000; i++) {
 			long k = random.nextLong();
-			Assert.assertEquals(k, (long)map.get(k));
+			Assertions.assertEquals(k, (long)map.get(k));
 			map = map.minus(k);
 		}
-		Assert.assertSame(PMap.empty(), map);
+		Assertions.assertSame(PMap.empty(), map);
 	}
 	
 	@Test
@@ -58,10 +58,10 @@ public class PMapTest {
 		random = new Random(seed);
 		for (int i = 0; i < 1000000; i++) {
 			long k = random.nextLong();
-			Assert.assertEquals(k, (long)map.get(k));
+			Assertions.assertEquals(k, (long)map.get(k));
 			map = map.minus(k);
 		}
-		Assert.assertSame(PMap.empty(), map);
+		Assertions.assertSame(PMap.empty(), map);
 	}
 	
 	<K, V> Map.Entry<K, V> entry(K k, V v) {
@@ -80,37 +80,37 @@ public class PMapTest {
 	public void testSecondaryMethods() {
 		PMap<Integer, Integer> map = PMap.<Integer, Integer>empty().plus(100, 1000).plus(200, 2000).plus(300, 3000);
 		
-		Assert.assertEquals("{200: 2000, 100: 1000, 300: 3000}", map.toString());
-		Assert.assertEquals(map, PMap.<Integer, Integer>empty().plus(200, 2000).plus(100, 1000).plus(300, 3000));
-		Assert.assertFalse(map.equals(PMap.<Integer, Integer>empty().plus(200, 2000).plus(100, 1000).plus(300, 3001)));
-		Assert.assertEquals(map, new HashMap<Integer, Integer>(map));
-		Assert.assertEquals(new HashMap<Integer, Integer>(map), map);
-		Assert.assertFalse(map.equals(new HashMap<Integer, Integer>()));
-		Assert.assertFalse(new HashMap<Integer, Integer>().equals(map));
-		Assert.assertEquals(new HashMap<Integer, Integer>(map).hashCode(), map.hashCode());
+		Assertions.assertEquals("{200: 2000, 100: 1000, 300: 3000}", map.toString());
+		Assertions.assertEquals(map, PMap.<Integer, Integer>empty().plus(200, 2000).plus(100, 1000).plus(300, 3000));
+		Assertions.assertFalse(map.equals(PMap.<Integer, Integer>empty().plus(200, 2000).plus(100, 1000).plus(300, 3001)));
+		Assertions.assertEquals(map, new HashMap<Integer, Integer>(map));
+		Assertions.assertEquals(new HashMap<Integer, Integer>(map), map);
+		Assertions.assertFalse(map.equals(new HashMap<Integer, Integer>()));
+		Assertions.assertFalse(new HashMap<Integer, Integer>().equals(map));
+		Assertions.assertEquals(new HashMap<Integer, Integer>(map).hashCode(), map.hashCode());
 		
-		Assert.assertTrue(map.containsKey(200));
-		Assert.assertFalse(map.containsKey(2000));
-		Assert.assertTrue(map.containsValue(3000));
-		Assert.assertFalse(map.containsValue(3001));
+		Assertions.assertTrue(map.containsKey(200));
+		Assertions.assertFalse(map.containsKey(2000));
+		Assertions.assertTrue(map.containsValue(3000));
+		Assertions.assertFalse(map.containsValue(3001));
 		
 		PList<Map.Entry<Integer, Integer>> entries = PList.empty();
 		entries = entries.plus(entry(100, 1000)).plus(entry(200, 2000)).plus(entry(300, 3000));
 		
-		Assert.assertEquals(toSet(entries), map.entrySet());
-		Assert.assertEquals(toSet(entries), toSet(toArrayList(map.entrySet())));
-		Assert.assertEquals(map.entrySet(), toSet(entries));
-		Assert.assertEquals(toSet(toArrayList(map.entrySet())), toSet(entries));
-		Assert.assertFalse(map.entrySet().isEmpty());
-		Assert.assertFalse(map.keySet().isEmpty());
-		Assert.assertFalse(map.values().isEmpty());
-		Assert.assertTrue(map.keySet().contains(200));
-		Assert.assertFalse(map.keySet().contains(201));
+		Assertions.assertEquals(toSet(entries), map.entrySet());
+		Assertions.assertEquals(toSet(entries), toSet(toArrayList(map.entrySet())));
+		Assertions.assertEquals(map.entrySet(), toSet(entries));
+		Assertions.assertEquals(toSet(toArrayList(map.entrySet())), toSet(entries));
+		Assertions.assertFalse(map.entrySet().isEmpty());
+		Assertions.assertFalse(map.keySet().isEmpty());
+		Assertions.assertFalse(map.values().isEmpty());
+		Assertions.assertTrue(map.keySet().contains(200));
+		Assertions.assertFalse(map.keySet().contains(201));
 		
-		Assert.assertEquals(toSet(PList.empty().plus(100).plus(200).plus(300)), map.keySet());
-		Assert.assertEquals(toSet(PList.empty().plus(1000).plus(2000).plus(3000)), toSet(map.values()));
+		Assertions.assertEquals(toSet(PList.empty().plus(100).plus(200).plus(300)), map.keySet());
+		Assertions.assertEquals(toSet(PList.empty().plus(1000).plus(2000).plus(3000)), toSet(map.values()));
 		
-		Assert.assertEquals(PMap.empty().plus(10, 100).plus(20, 200).plus(30, 300), PMap.empty().plus(10, 100).plusAll(PMap.empty().plus(20, 200).plus(30, 300)));
+		Assertions.assertEquals(PMap.empty().plus(10, 100).plus(20, 200).plus(30, 300), PMap.empty().plus(10, 100).plusAll(PMap.empty().plus(20, 200).plus(30, 300)));
 	}
 	
 }
